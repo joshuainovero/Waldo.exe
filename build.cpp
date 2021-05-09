f0356c4b06a9531c13b9d4665af99ae65f36ff21
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-05-09 21:06:10 +0800

    added

--- build.cpp
+++ build.cpp
@@ -1,12 +1,13 @@
+//VS code extension | Code runner - Exectutor map - settings.json
+//File -> Preferences -> Settings -> Search executor -> Executor Map | settings.json
+ //"cpp": "g++ build.cpp -o build.exe && start build.exe"
+
 #include <Windows.h>
 #include <iostream>
 
 int main(){
-    //VS code extension | Code runner - Executor map -> settings.json:
-    //"cpp": "g++ build.cpp -o build.exe && start build.exe"
     system("mingw32-make");
     system("start main.exe");
-    
     std::cin.get();
     return 0;
 }
