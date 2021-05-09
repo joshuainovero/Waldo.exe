f76776d96f56591c535c2213de683a9ae9997a6f
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-05-09 21:15:21 +0800

    added

--- build.cpp
+++ build.cpp
@@ -1,6 +1,6 @@
 //VS code extension | Code runner - Exectutor map - settings.json
 //File -> Preferences -> Settings -> Search executor -> Executor Map | settings.json
- //"cpp": "g++ build.cpp -o build.exe && start build.exe"
+//"cpp": "g++ build.cpp -o build.exe && start build.exe"
 
 #include <Windows.h>
 #include <iostream>
