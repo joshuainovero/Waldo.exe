2d0ee823612595dd619ad0aa1785ed404c799247
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-05-11 14:24:05 +0800

    added core

---
+++ src/Core.hpp
@@ -0,0 +1,12 @@
+#ifndef CORE
+#define CORE
+
+#include "Scales.hpp"
+#include "Labels.hpp"
+#include "MapProperties.hpp"
+#include "loadAssets.hpp"
+#include "GameStates.hpp"
+#include "GameMenu.hpp"
+#include "InGame.hpp"
+
+#endif //CORE
