da605d121153bc341ececfa6be2976aa506bf1c2
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-06-07 19:11:55 +0800

    improve build

--- build.bat
+++ build.bat
@@ -1,4 +1,25 @@
 @echo off
+setlocal enabledelayedexpansion
+echo Build started at: %cd% && echo.
 mingw32-make
-start Build/release/Waldo.exe
+IF %ERRORLEVEL% NEQ 0 ( 
+    echo Compilation error. && pause
+    exit 0
+) else (
+echo. && echo The build was succesfully executed.
+echo Cleaning files...
+mingw32-make clean
+if exist *.o (
+    echo Failed to clean the files...
+) else ( 
+    echo Cleaned .o files.
+)
+
+set /p ans=The build was successful. Do you want to run? [y/n]:
+if !ans! == y (
+    echo Starting app...
+    start Build/release/Waldo.exe
+)
+
+)
 pause
