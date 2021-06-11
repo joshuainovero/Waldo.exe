f2aaca5e3ce005ee55aee37121ca58650971725b
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-06-11 22:24:23 +0800

    implement inheritance in asset loading

--- src/loadAssets.hpp
+++ src/loadAssets.hpp
@@ -1,136 +1,112 @@
 #pragma once
 
-const float fullScreenSprite720 = 0.714f;
-const float fullScreenSprite1080 = 1;
-
-//Jumpscare pictures
-struct structJumpscare {
+struct SpriteLoader {
 	sf::Texture texture;
 	sf::Sprite sprite;
-	structJumpscare(std::string path) {
+	virtual void SetSpriteScale() = 0;
+	SpriteLoader(const std::string &path){
 		texture.loadFromFile(path);
 		sprite.setTexture(texture);
-		sprite.setScale(sf::Vector2f(0.714f, 0.714f));
 	}
 };
 
-//Music
-struct SoundEffect {
+struct ARROWCURSOR : public SpriteLoader {
+	virtual void SetSpriteScale() override {
+		sprite.setScale(sf::Vector2f(scaleresol.getScaleArrow(), scaleresol.getScaleArrow()));
+	}
+	ARROWCURSOR(const std::string &pathA)
+		:SpriteLoader(pathA){
+			SetSpriteScale();
+			sprite.setOrigin(68, 23);
+		}
+};
+
+struct HANDCURSOR : public SpriteLoader {
+	virtual void SetSpriteScale() override {
+		sprite.setScale(sf::Vector2f(scaleresol.getScaleArrow(), scaleresol.getScaleArrow()));
+	}
+	HANDCURSOR(const std::string &pathA)
+		:SpriteLoader(pathA){
+			SetSpriteScale();
+			sprite.setOrigin(110, 30);
+		}
+};
+
+struct CIRCLECURSOR : public SpriteLoader {
+	StaticObjAnimation AnimationCircle;
+	virtual void SetSpriteScale() override {
+		sprite.setScale(sf::Vector2f(scaleresol.getScaleCircleCursor(), scaleresol.getScaleCircleCursor()));
+	}
+	CIRCLECURSOR(const std::string &pathA)
+		:SpriteLoader(pathA){
+			SetSpriteScale();
+			sprite.setOrigin(187, 180);
+			AnimationCircle = StaticObjAnimation(&texture, sf::Vector2u(9,1), 0.06f);
+		}
+};
+
+struct EXITICON : public SpriteLoader {
+	int clickableX[2], clickableY[2];
+	float exitWidth, exitHeight;
+	virtual void SetSpriteScale() override {
+		sprite.setScale(sf::Vector2f(scaleresol.getScaleExitIcon(), scaleresol.getScaleExitIcon()));
+	}
+	EXITICON(const std::string &pathA)
+		:SpriteLoader(pathA){
+			SetSpriteScale();
+			exitWidth = (sprite.getTexture()->getSize().x) * scaleresol.getScaleExitIcon();
+			exitHeight = (sprite.getTexture()->getSize().y) * scaleresol.getScaleExitIcon();
+			sprite.setOrigin(sprite.getTexture()->getSize().x, 0);
+			clickableX[0] = (sf::VideoMode::getDesktopMode().width) - exitWidth;
+			clickableX[1] = sf::VideoMode::getDesktopMode().width;
+			clickableY[0] = 0;
+			clickableY[1] = exitHeight;
+			sprite.setPosition(sf::VideoMode::getDesktopMode().width, 0);
+		}
+};
+
+struct MENULOGO : public SpriteLoader {
+	virtual void SetSpriteScale() override {
+		sprite.setScale(sf::Vector2f(scaleresol.getFullScreenSpriteScale(), scaleresol.getFullScreenSpriteScale()));
+	}
+	MENULOGO(const std::string &pathA)
+		:SpriteLoader(pathA){
+			SetSpriteScale();
+		}
+};
+
+struct SOUNDEFFECT {
 	sf::SoundBuffer buffer;
 	sf::Sound sound;
-	SoundEffect(std::string path) {
+	SOUNDEFFECT(const std::string &path){
 		buffer.loadFromFile(path);
 		sound.setBuffer(buffer);
 	}
 };
 
-struct GameMusic {
+struct GAMEMUSIC {
 	sf::Music music;
-	GameMusic(std::string path) {
+	GAMEMUSIC(const std::string &path){
 		music.openFromFile(path);
 	}
 };
 
-//CircleCursor
-struct structCircle {
-	sf::Texture circleTexture;
-	sf::Sprite circleSprite;
-	StaticObjAnimation AnimationCircle;
-	structCircle()
-	:AnimationCircle() {
-		circleTexture.loadFromFile("Assets/Cursors/redcirclesprite.png");
-		circleSprite.setTexture(circleTexture);
-		circleSprite.setScale(sf::Vector2f(scaleresol.getScaleCircleCursor(), scaleresol.getScaleCircleCursor()));
-		circleSprite.setOrigin(187, 180);
-		AnimationCircle = StaticObjAnimation(&circleTexture, sf::Vector2u(9,1), 0.06f);
-	}
-};
+// ASSETS
+ARROWCURSOR ArrowCursorObj("Assets/Cursors/ArrowCursor.png");
+HANDCURSOR HandCursorObj("Assets/Cursors/HandCursor.png");
+CIRCLECURSOR CircleCursorObj("Assets/Cursors/redcirclesprite.png");
+EXITICON ExitIconObj("Assets/Icon/InIcons/exit.png");
+MENULOGO MenuLogoObj("Assets/Maps/MenuUI.png");
+
+GAMEMUSIC Intromusic("Assets/Audio/music/MenuMusic.wav");
+SOUNDEFFECT WrongClickEffect("Assets/Audio/SoundEffects/WrongClick.wav");
+SOUNDEFFECT WallyFoundEffect("Assets/Audio/SoundEffects/WallyFound.wav");
 
 
-//Arrow Cursor
-struct structArrow {
-	sf::Texture arrowCursorTexture;
-	sf::Sprite  arrowCursorSprite;
-
-	structArrow() {
-		arrowCursorTexture.loadFromFile("Assets/Cursors/ArrowCursor.png");
-		arrowCursorSprite.setTexture(arrowCursorTexture);
-		//0.30f for 768p
-		// std::cout << "scale arrow : " << scaleresol.getScaleArrow() << std::endl;
-		arrowCursorSprite.setScale(sf::Vector2f(scaleresol.getScaleArrow(), scaleresol.getScaleArrow()));
-		arrowCursorSprite.setOrigin(68, 23);
-	}
-};
-
-
-//Hand Cursor
-struct structHandCursor {
-	sf::Texture handCursorTexture;
-	sf::Sprite  handCursorSprite;
-
-	structHandCursor() {
-		handCursorTexture.loadFromFile("Assets/Cursors/HandCursor.png");
-		handCursorSprite.setTexture(handCursorTexture);
-		handCursorSprite.setScale(sf::Vector2f(scaleresol.getScaleArrow(), scaleresol.getScaleArrow()));
-		handCursorSprite.setOrigin(110, 30);
-	}
-};
-
-//Exit icon
-struct structExitIcon {
-	sf::Texture exitIconTexture;
-	sf::Sprite exitIconSprite;
-	int clickableX[2], clickableY[2];
-	float exitWidth, exitHeight;
-	structExitIcon(){
-		exitIconTexture.loadFromFile("Assets/Icon/InIcons/exit.png");
-		exitIconSprite.setTexture(exitIconTexture);
-		exitWidth = (exitIconSprite.getTexture()->getSize().x) * scaleresol.getScaleExitIcon();
-		exitHeight = (exitIconSprite.getTexture()->getSize().y) * scaleresol.getScaleExitIcon();
-		exitIconSprite.setScale(sf::Vector2f(scaleresol.getScaleExitIcon(), scaleresol.getScaleExitIcon()));
-		exitIconSprite.setOrigin(exitIconSprite.getTexture()->getSize().x, 0);
-		
-		clickableX[0] = (sf::VideoMode::getDesktopMode().width) - exitWidth;
-		clickableX[1] = sf::VideoMode::getDesktopMode().width;
-		clickableY[0] = 0;
-		clickableY[1] = exitHeight;
-		exitIconSprite.setPosition(sf::VideoMode::getDesktopMode().width, 0);
-	}
-};
-
-
-//Menu
-struct structMenu {
-	sf::Texture textureMenuLogo;
-	sf::Sprite spriteMenuLogo;
-
-	structMenu() {
-		textureMenuLogo.loadFromFile("Assets/Maps/MenuUI.png");
-		spriteMenuLogo.setTexture(textureMenuLogo);
-		spriteMenuLogo.setScale(sf::Vector2f(scaleresol.getFullScreenSpriteScale(), scaleresol.getFullScreenSpriteScale()));
-	}
-};
-
-
-
-structMenu MenuLogo;
-structExitIcon ExitIcon;
-structArrow ArrowCursor;
-structHandCursor HandCursor;
-structCircle circleCursor;
-
-GameMusic intro("Assets/Audio/music/MenuMusic.wav");
-SoundEffect WrongClick("Assets/Audio/SoundEffects/WrongClick.wav");
-SoundEffect WallyFound("Assets/Audio/SoundEffects/WallyFound.wav");
-SoundEffect Scream("Assets/Audio/SoundEffects/Scream.wav");
-
-structJumpscare Jumpscare1("Assets/Jumpscares/Jumpscare1.png");
-
-
-//Maps
-Map1 MAP1("Assets/Maps/Map1.png");
-Map2 MAP2("Assets/Maps/Map2.png");
-Map3 MAP3("Assets/Maps/Map3.png");
-Map4 MAP4("Assets/Maps/Map4.png");
-Map5 MAP5("Assets/Maps/Map5.png");
-Map6 MAP6("Assets/Maps/Map6.png");
+// MAPS
+MapProperty MAP1("Assets/Maps/Map1.png", "Map1", GameTimer::m1TimeCounts);
+MapProperty MAP2("Assets/Maps/Map2.png", "Map2", GameTimer::m2TimeCounts);
+MapProperty MAP3("Assets/Maps/Map3.png", "Map3", GameTimer::m3TimeCounts);
+MapProperty MAP4("Assets/Maps/Map4.png", "Map4", GameTimer::m4TimeCounts);
+MapProperty MAP5("Assets/Maps/Map5.png", "Map5", GameTimer::m5TimeCounts);
+MapProperty MAP6("Assets/Maps/Map6.png", "Map6", GameTimer::m6TimeCounts);
