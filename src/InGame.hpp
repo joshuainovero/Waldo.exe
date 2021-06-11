4d3e131fe4c23c9909fd2524c960536aae17e833
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-06-11 22:26:47 +0800

    remove unecessary booleans

--- src/InGame.hpp
+++ src/InGame.hpp
@@ -1,174 +1,60 @@
 #pragma once
 
-//std::string textContent, float textPosX, float textPosY, unsigned int charSize
-//std::string textContent, float textPosX, float textPosY, unsigned int charSize, sf::Color
-TextLabel timerDisplaySeconds("-", sf::VideoMode::getDesktopMode().width / 2, 40.0, 65, sf::Color::White);
-TextLabel timerDisplayColon(":", (sf::VideoMode::getDesktopMode().width / 2) - 20, 40.0, 65, sf::Color::White);
-TextLabel timerDisplayMinutes("-", (sf::VideoMode::getDesktopMode().width / 2) - (65 + 20) + 35, 40.0, 65, sf::Color::White);
-
-//Shadow Properties
-TextLabel timerShadowSeconds("-", (sf::VideoMode::getDesktopMode().width / 2) + 5, 45.0, 66, sf::Color::Black);
-TextLabel timerShadowColon(":", (sf::VideoMode::getDesktopMode().width / 2) - 15, 45.0, 66, sf::Color::Black);
-TextLabel timerShadowMinutes("-", ((sf::VideoMode::getDesktopMode().width / 2) - (65 + 20) + 35) + 5, 45.0, 66, sf::Color::Black);
-
-static bool movedText = false; //Prevents the loop to not keep moving
-static bool waitRunning = false;
-static bool ScreamOn = false;
-static bool EnableClick = false;
-static bool isOver = false;
-static bool wallyFound = false;
-static bool textMoved = false;
-
-sf::Clock* wait;
 sf::Vector2i inGameMousePos;
-const int startingTime = 60;
-int countdown = 0;
-int tempSeconds = 0;
-
-void resetMapState() {
-	//Reset inGame properties
-	movedText = false; //Prevents the loop to not keep moving
-	waitRunning = false;
-	EnableClick = false;
-	countdown = 0;
-	tempSeconds = 0;
-	isOver = false;
-	ScreamOn = false;
-	wallyFound = false;
-	delete wait;
-}
+static bool readyForClick = false;
 
 void InGame(sf::RenderWindow *inGameWindow) {
-	circleCursor.AnimationCircle.Update(circleCursor.AnimationCircle.currentRow, deltaTime);
-	circleCursor.circleSprite.setTextureRect(circleCursor.AnimationCircle.uvRect);
-	//Responsive Timer -> Start
-	if (CurrentMap->clockRunning) {
-		if (countdown == 59 && CurrentMap->getMapMinutes() == -1) {
-			countdown++;
-			CurrentMap->clockRunning = false;
-			isOver = true;
-			delete CurrentMap->clockTimer;
-		}
-		if (CurrentMap->getMapMinutes() > 1 && !textMoved) {
-			textMoved = true;
-			timerDisplayMinutes.returnText()->setPosition((sf::VideoMode::getDesktopMode().width / 2) - (65 + 20) + 27, 40.0);
-			timerShadowMinutes.returnText()->setPosition(((sf::VideoMode::getDesktopMode().width / 2) - (65 + 20) + 27) + 5, 45.0);
-		}
-		//Move Text -> Start
-		else if (CurrentMap->getMapMinutes() <= 1 && textMoved) {
-			textMoved = false;
-			timerDisplayMinutes.returnText()->setPosition((sf::VideoMode::getDesktopMode().width / 2) - (65 + 20) + 35, 40.0);
-			timerShadowMinutes.returnText()->setPosition(((sf::VideoMode::getDesktopMode().width / 2) - (65 + 20) + 35) + 5, 45.0);
-		}
-		if (CurrentMap->getMapMinutes() != 0 && !isOver) {
-			timerDisplayMinutes.returnText()->setString(std::to_string(CurrentMap->getMapMinutes()));
-			timerShadowMinutes.returnText()->setString(std::to_string(CurrentMap->getMapMinutes()));
-		}
-		//Move Text -> End
+	CircleCursorObj.AnimationCircle.Update(CircleCursorObj.AnimationCircle.currentRow, deltaTime);
+	CircleCursorObj.sprite.setTextureRect(CircleCursorObj.AnimationCircle.uvRect);
 
-		//Update Countdown -> Start
-		sf::Time elapsed = CurrentMap->clockTimer->getElapsedTime();
-		if (static_cast<int>(elapsed.asSeconds()) != tempSeconds) {
-			tempSeconds = static_cast<int>(elapsed.asSeconds());
-			countdown = startingTime - tempSeconds;
-			if (countdown == 59) {
-				CurrentMap->decrementtMapMinutes();
-			}
-			if (CurrentMap->getMapMinutes() == 0) {
-				//CurrentMap->decrementtMapMinutes();
-				timerDisplayMinutes.returnText()->setString("");
-				timerShadowMinutes.returnText()->setString("");
-			}
-		}
-		//Update Countdown -> End
-
-		if (countdown >= 10) {
-			if (countdown == 60 || (countdown == 59 && CurrentMap->getMapMinutes() == -1)) {
-				timerDisplaySeconds.returnText()->setString("00");
-				timerShadowSeconds.returnText()->setString("00");
-			}
-			else {
-				timerDisplaySeconds.returnText()->setString(std::to_string(countdown));
-				timerShadowSeconds.returnText()->setString(std::to_string(countdown));
-			}
-		}
-		else {
-			if (countdown == 0 && CurrentMap->getMapMinutes() != 1)
-				CurrentMap->clockTimer->restart();
-
-			else if (countdown >= 0 && countdown != 60){
-				timerDisplaySeconds.returnText()->setString("0" + std::to_string(countdown));
-				timerShadowSeconds.returnText()->setString("0" + std::to_string(countdown));
-			}
-					
-
-		}
-
-
-	}
-	//Responsive Timer -> End
+	if (delayInGameClick >= CurrentMap->gametimer.seconds)
+		readyForClick = true;
 
 	inGameWindow->setMouseCursorVisible(false);
 	
-	
 	if (appInFocus(inGameWindow)){
 		inGameMousePos = sf::Mouse::getPosition(*inGameWindow);
-		circleCursor.circleSprite.setPosition(static_cast<float>(inGameMousePos.x), static_cast<float>(inGameMousePos.y));
-
-		if (EnableClick) { //Delays
+		CircleCursorObj.sprite.setPosition(static_cast<float>(inGameMousePos.x), static_cast<float>(inGameMousePos.y));
+		if (readyForClick){
 			if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) { // If wally is found
-				if ((inGameMousePos.x >= CurrentMap->getWallyx1() && inGameMousePos.x <= CurrentMap->getWallyx2()) &&
-					(inGameMousePos.y >= CurrentMap->getWallyy1() && inGameMousePos.y <= CurrentMap->getWallyy2())) {
-					WallyFound.sound.play();
-					wallyFound = true;
-					if (CurrentMap->clockRunning) {
-						CurrentMap->clockRunning = false;
-						delete CurrentMap->clockTimer;
+				if (CurrentMap->checkMouseClick(inGameMousePos)) {
+						WallyFoundEffect.sound.play();
+						CurrentMap->waldoFound = true;
+						if (GameClockStruct::ClockRunning) {
+							GameClockStruct::ClockRunning = false;
+							//delete CurrentMap->clockTimer;
+						}
 					}
+					else WrongClickEffect.sound.play();
 				}
-				else WrongClick.sound.play();
-			}
 		}
+		
 	}
+	if (CurrentMap->gametimer.seconds == -1 && CurrentMap->gametimer.minutes == 0){
+		CurrentMap->gametimer.stopTimer();
+		CurrentMap->gameOver = true;
+	}
+	
+	if (GameClockStruct::ClockRunning)
+		CurrentMap->gametimer.UpdateTimer();
 
-	if (static_cast<int>(CurrentMap->clockTimer->getElapsedTime().asSeconds()) == 1) EnableClick = true; //Delay for clicking
 	inGameWindow->draw(CurrentMap->returnSprite());
-	inGameWindow->draw(circleCursor.circleSprite);
-
-	//Shadow Timer
-	inGameWindow->draw(*timerShadowSeconds.returnText());
-	inGameWindow->draw(*timerShadowColon.returnText());
-	inGameWindow->draw(*timerShadowMinutes.returnText());
+	inGameWindow->draw(CircleCursorObj.sprite);
+	CurrentMap->gametimer.drawTimer(inGameWindow);
 
 
-	//Actual Timer
-	inGameWindow->draw(*timerDisplaySeconds.returnText());
-	inGameWindow->draw(*timerDisplayColon.returnText());
-	inGameWindow->draw(*timerDisplayMinutes.returnText());
-	//inGameWindow->draw(*timerBG.getButton());
+	if (CurrentMap->gameOver) {
+		CurrentMap->resetMapState();
+		MenuActive = true;
+		InGameActive = false;
+		readyForClick = false;
 
-	if (isOver) { //Gameover
-		//inGameWindow->draw(Jumpscare1.sprite);
-		//if (!ScreamOn) {
-		//	Scream.sound.play();
-		//	ScreamOn = true;
-		//}
-		CurrentMap->resetMapMinutes();
-		if (!waitRunning) {
-			wait = new sf::Clock;
-			waitRunning = true;
-		}
-		else {
-			sf::Time waitElapse = wait->getElapsedTime();
-			if (static_cast<int>(waitElapse.asSeconds()) == 10) {
-				resetMapState();
-				MenuActive = true;
-				InGameActive = false;
-			}
-		}
 	}
-	if (wallyFound) {
-		resetMapState();
+
+	if (CurrentMap->waldoFound) {
+		GameClockStruct::gameClockTimer.restart();
+		readyForClick = false;
+		CurrentMap->resetMapState();
 		updateCurrentMapOrder();
 		SetMapProperty();
 	}
