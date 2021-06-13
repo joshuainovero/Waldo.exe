b79c6349eccffec6cdc6ee0c9e3a1f203423a625
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-06-13 22:26:54 +0800

    add pause screen asset

--- src/loadAssets.hpp
+++ src/loadAssets.hpp
@@ -65,6 +65,43 @@ struct EXITICON : public SpriteLoader {
 		}
 };
 
+struct PAUSESCREEN : public SpriteLoader {
+	sf::Vector2i screenSize;
+	int rRangeClickX[2], rRangeClickY[2];
+	//615 -> 791         353 -> 403
+	int qRangeClickX[2], qRangeClickY[2];
+	//615 -> 791         460 -> 510
+	virtual void SetSpriteScale() override {
+		sprite.setScale(sf::Vector2f(scaleresol.getFullScreenSpriteScale(), scaleresol.getFullScreenSpriteScale()));
+		screenSize.x = sf::VideoMode::getDesktopMode().width;
+		screenSize.y = sf::VideoMode::getDesktopMode().height;
+
+		// Range Resume
+		rRangeClickX[0] = screenSize.x * 0.4502196193;
+		rRangeClickX[1] = screenSize.x * 0.5790629575;
+		rRangeClickY[0] = screenSize.y * 0.4596354167;
+		rRangeClickY[1] = screenSize.y * 0.5247395833;
+
+		//Range Quit
+		qRangeClickX[0] = screenSize.x * 0.4502196193;
+		qRangeClickX[1] = screenSize.x * 0.5790629575;
+		qRangeClickY[0] = screenSize.y * 0.5989583333;
+		qRangeClickY[1] = screenSize.y * 0.6640625;
+	}
+	PAUSESCREEN(const std::string &pathA)
+		:SpriteLoader(pathA){
+			SetSpriteScale();
+		}
+	bool resumeInRange(const sf::Vector2i &mousePos){
+		return ((mousePos.x >= rRangeClickX[0] && mousePos.x <= rRangeClickX[1]) && 
+			(mousePos.y >= rRangeClickY[0] && mousePos.y <= rRangeClickY[1]));
+	}
+	bool quitInRange(const sf::Vector2i &mousePos) {
+		return ((mousePos.x >= qRangeClickX[0] && mousePos.x <= qRangeClickX[1]) &&
+			(mousePos.y >= qRangeClickY[0] && mousePos.y <= qRangeClickY[1]));
+	}
+};
+
 struct MENULOGO : public SpriteLoader {
 	virtual void SetSpriteScale() override {
 		sprite.setScale(sf::Vector2f(scaleresol.getFullScreenSpriteScale(), scaleresol.getFullScreenSpriteScale()));
@@ -97,6 +134,7 @@ HANDCURSOR HandCursorObj("Assets/Cursors/HandCursor.png");
 CIRCLECURSOR CircleCursorObj("Assets/Cursors/redcirclesprite.png");
 EXITICON ExitIconObj("Assets/Icon/InIcons/exit.png");
 MENULOGO MenuLogoObj("Assets/Maps/MenuUI.png");
+PAUSESCREEN PauseScreenObj("Assets/Maps/Pause.png");
 
 GAMEMUSIC Intromusic("Assets/Audio/music/MenuMusic.wav");
 SOUNDEFFECT WrongClickEffect("Assets/Audio/SoundEffects/WrongClick.wav");
