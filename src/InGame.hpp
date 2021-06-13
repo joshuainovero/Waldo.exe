7dcfb07c02b0b6755484cac9d63ef5a430992284
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-06-14 01:23:30 +0800

    add gameover sprite

--- src/InGame.hpp
+++ src/InGame.hpp
@@ -28,7 +28,10 @@ void InGame(sf::RenderWindow *inGameWindow) {
 				inGameWindow->setMouseCursorVisible(true);
 				if (MessageBoxA(NULL,"Are you sure you want to go to the main menu?", "Waldo", MB_YESNO) == IDYES){
 					CurrentMap->gametimer.stopTimer();
-					CurrentMap->gameOver = true;
+				 	CurrentMap->resetMapState();
+			 		TimerLabel::moveTextMinutes = false;
+			 		MenuActive = true;
+			 		InGameActive = false;
 					gamePause = false;
 				}
 			}
@@ -45,63 +48,111 @@ void InGame(sf::RenderWindow *inGameWindow) {
 		}  else keyDown = false;
 		GameClockStruct::gameClockTimer.restart();
 	} else {
-		CircleCursorObj.AnimationCircle.Update(CircleCursorObj.AnimationCircle.currentRow, deltaTime);
-		CircleCursorObj.sprite.setTextureRect(CircleCursorObj.AnimationCircle.uvRect);
-		
-		if (appInFocus(inGameWindow)){
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
-				if (!keyDown){
-					gamePause = true;
-					keyDown = true;
-				}
-			} else {
-				keyDown = false;
-			}
+		if (CurrentMap->waldoFound){
 			inGameMousePos = sf::Mouse::getPosition(*inGameWindow);
-			CircleCursorObj.sprite.setPosition(static_cast<float>(inGameMousePos.x), static_cast<float>(inGameMousePos.y));
-			
-			if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) { // If wally is found
-				if (!mouseDown){
-					if (CurrentMap->checkMouseClick(inGameMousePos)) {
-						WallyFoundEffect.sound.play();
-						CurrentMap->waldoFound = true;
-						if (GameClockStruct::ClockRunning) {
-							GameClockStruct::ClockRunning = false;
-						}
+			inGameWindow->draw(CurrentMap->returnSprite());
+			CurrentMap->gametimer.drawTimer(inGameWindow);
+			inGameWindow->draw(WaldoFoundObj.sprite);
+			if (WaldoFoundObj.continueInRange(inGameMousePos)){
+				HandCursorObj.sprite.setPosition(static_cast<float>(inGameMousePos.x), static_cast<float>(inGameMousePos.y));
+				inGameWindow->draw(HandCursorObj.sprite);
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+					if (!mouseDown){
+						GameClockStruct::gameClockTimer.restart();
+						TimerLabel::moveTextMinutes = false;
+						CurrentMap->resetMapState();
+						updateCurrentMapOrder();
+						SetMapProperty();
+						mouseDown = true;
 					}
-					else WrongClickEffect.sound.play();
-					mouseDown = true;
+				} else
+					mouseDown = false;
+			} else {
+				ArrowCursorObj.sprite.setPosition(static_cast<float>(inGameMousePos.x), static_cast<float>(inGameMousePos.y));
+				inGameWindow->draw(ArrowCursorObj.sprite);
+			}
+		} else if (CurrentMap->gameOver){
+			inGameMousePos = sf::Mouse::getPosition(*inGameWindow);
+			inGameWindow->draw(CurrentMap->returnSprite());
+			CurrentMap->gametimer.drawTimer(inGameWindow);
+			inGameWindow->draw(GameOverObj.sprite);
+			if (GameOverObj.continueInRange(inGameMousePos)){
+				HandCursorObj.sprite.setPosition(static_cast<float>(inGameMousePos.x), static_cast<float>(inGameMousePos.y));
+				inGameWindow->draw(HandCursorObj.sprite);
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+					if (!mouseDown){
+						CurrentMap->resetMapState();
+						TimerLabel::moveTextMinutes = false;
+						MenuActive = true;
+						InGameActive = false;
+						mouseDown = true;
+					}
+				} else
+					mouseDown = false;
+			} else {
+				ArrowCursorObj.sprite.setPosition(static_cast<float>(inGameMousePos.x), static_cast<float>(inGameMousePos.y));
+				inGameWindow->draw(ArrowCursorObj.sprite);
+			}
+
+		} else {
+			CircleCursorObj.AnimationCircle.Update(CircleCursorObj.AnimationCircle.currentRow, deltaTime);
+			CircleCursorObj.sprite.setTextureRect(CircleCursorObj.AnimationCircle.uvRect);
+			
+			if (appInFocus(inGameWindow)){
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+					if (!keyDown){
+						gamePause = true;
+						keyDown = true;
+					}
+				} else {
+					keyDown = false;
 				}
-			} else
-				mouseDown = false;		
-		}
-		if (CurrentMap->gametimer.seconds == -1 && CurrentMap->gametimer.minutes == 0){
-			CurrentMap->gametimer.stopTimer();
-			CurrentMap->gameOver = true;
-		}
-		
-		if (GameClockStruct::ClockRunning)
-			CurrentMap->gametimer.UpdateTimer();
+				inGameMousePos = sf::Mouse::getPosition(*inGameWindow);
+				CircleCursorObj.sprite.setPosition(static_cast<float>(inGameMousePos.x), static_cast<float>(inGameMousePos.y));
+				
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) { // If wally is found
+					if (!mouseDown){
+						if (CurrentMap->checkMouseClick(inGameMousePos)) {
+							WallyFoundEffect.sound.play();
+							CurrentMap->waldoFound = true;
+							if (GameClockStruct::ClockRunning) {
+								GameClockStruct::ClockRunning = false;
+							}
+						}
+						else WrongClickEffect.sound.play();
+						mouseDown = true;
+					}
+				} else
+					mouseDown = false;		
+			}
+			if (CurrentMap->gametimer.seconds == -1 && CurrentMap->gametimer.minutes == 0){
+				CurrentMap->gametimer.stopTimer();
+				CurrentMap->gameOver = true;
+			}
+			
+			if (GameClockStruct::ClockRunning)
+				CurrentMap->gametimer.UpdateTimer();
 
-		inGameWindow->draw(CurrentMap->returnSprite());
-		inGameWindow->draw(CircleCursorObj.sprite);
-		CurrentMap->gametimer.drawTimer(inGameWindow);
+			inGameWindow->draw(CurrentMap->returnSprite());
+			inGameWindow->draw(CircleCursorObj.sprite);
+			CurrentMap->gametimer.drawTimer(inGameWindow);
 
 
-		if (CurrentMap->gameOver) {
-			CurrentMap->resetMapState();
-			TimerLabel::moveTextMinutes = false;
-			MenuActive = true;
-			InGameActive = false;
+			// if (CurrentMap->gameOver) {
+			// 	CurrentMap->resetMapState();
+			// 	TimerLabel::moveTextMinutes = false;
+			// 	MenuActive = true;
+			// 	InGameActive = false;
 
-		}
+			// }
 
-		if (CurrentMap->waldoFound) {
-			GameClockStruct::gameClockTimer.restart();
-			TimerLabel::moveTextMinutes = false;
-			CurrentMap->resetMapState();
-			updateCurrentMapOrder();
-			SetMapProperty();
+			// if (CurrentMap->waldoFound) {
+			// 	GameClockStruct::gameClockTimer.restart();
+			// 	TimerLabel::moveTextMinutes = false;
+			// 	CurrentMap->resetMapState();
+			// 	updateCurrentMapOrder();
+			// 	SetMapProperty();
+			// }
 		}
 	}
 
