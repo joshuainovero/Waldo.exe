9fca448a97ce3e7972cdfd6313a4c93a7d867d6e
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-07-05 01:24:35 +0800

    add TypeWriter header to core

--- src/Core.hpp
+++ src/Core.hpp
@@ -7,8 +7,10 @@
 #include "Labels.hpp"
 #include "MapProperties.hpp"
 #include "StaticObjAnimation.hpp"
+#include "TypeWriter.hpp"
 #include "loadAssets.hpp"
 #include "GameStates.hpp"
+#include "MapSelect.hpp"
 #include "GameMenu.hpp"
 #include "InGame.hpp"
 
