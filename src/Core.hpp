2c0f76a8ad27f99c6dabe10794a9c0a68bf743c0
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-07-07 02:32:01 +0800

    update core.hpp

--- src/Core.hpp
+++ src/Core.hpp
@@ -1,17 +1,35 @@
 #ifndef CORE_H
 #define CORE_H
 
+// SFML
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+// Subsystem
+#include <Windows.h>
+
+// STD
+#include <json/json.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unistd.h>
+#include <direct.h>
+#include <memory>
+
+// SRC
 #include "Filemanip.hpp"
 #include "Scales.hpp"
 #include "Labels.hpp"
-#include "MapProperties.hpp"
+// #include "MapProperties.hpp"
+#include "Engine.hpp"
 #include "StaticObjAnimation.hpp"
 #include "TypeWriter.hpp"
 #include "loadAssets.hpp"
-#include "GameStates.hpp"
+#include "MapConfigurator.hpp"
 #include "MapSelect.hpp"
 #include "GameMenu.hpp"
 #include "InGame.hpp"
+
 
 #endif //CORE_H
