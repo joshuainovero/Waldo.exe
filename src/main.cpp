a0d4241374974c3faa37165b3a1a66e27f2b80f9
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-07-07 02:31:40 +0800

    transfer STD header files to core

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,12 @@
-#include <Windows.h>
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <json/json.h>
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <unistd.h>
-#include <direct.h>
-
-float deltaTime = 0.0f;
-bool mouseDown = true;
 #include "Core.hpp"
 
 int WINAPI WinMain(HINSTANCE hThisInstance, HINSTANCE hPrevInstance,LPSTR lpszArgument, int nCmdShow){
+	State::states.push_back(new GAMEMENU);
+	State::states.push_back(new MAPSELECT);
+	State::states.push_back(new INGAME);
 	windowLoad();
 	getMainDirectory();
-	std::ofstream emptySpriteFile("Assets/sprite.png", std::ios::binary);
-	sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height), "Where's Wally?", sf::Style::Close | sf::Style::Fullscreen);
-	window.setVerticalSyncEnabled(true);
-	sf::Clock animationClock;
-	while (window.isOpen()) {
-		if (!GameClockStruct::ClockRunning)
-			GameClockStruct::gameClockTimer.restart();
-		deltaTime = animationClock.restart().asSeconds();
-		window.clear();
-		if (MenuActive && !InGameActive && !MapSelectActive) 
-			GameMenu(&window);
-		else if (MapSelectActive && !MenuActive && !InGameActive)
-			MapSelection(&window);
-		else if (InGameActive && !MenuActive && !MapSelectActive) 
-			InGame(&window);
-		
-		gameEvents(&window);
-		window.display();
-	}
+	Engine engine;
+	engine.run();
 	return EXIT_SUCCESS;
 }
