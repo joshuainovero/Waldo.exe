7367edb99a8c01a06701c09a0e3d5229d846f665
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-07-07 19:27:58 +0800

    update core

--- src/Core.hpp
+++ src/Core.hpp
@@ -4,24 +4,14 @@
 // SFML
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
 
 // Subsystem
 #include <Windows.h>
 
-// STD
-#include <json/json.h>
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <unistd.h>
-#include <direct.h>
-#include <memory>
-
-// SRC
 #include "Filemanip.hpp"
 #include "Scales.hpp"
 #include "Labels.hpp"
-// #include "MapProperties.hpp"
 #include "Engine.hpp"
 #include "StaticObjAnimation.hpp"
 #include "TypeWriter.hpp"
