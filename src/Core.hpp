72b1df236083d19b3d5a997a4543bb590e476166
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-07-08 19:34:04 +0800

    remove MapConfigurator.hpp & Scales.hpp

--- src/Core.hpp
+++ src/Core.hpp
@@ -10,16 +10,14 @@
 #include <Windows.h>
 
 #include "Filemanip.hpp"
-#include "Scales.hpp"
 #include "Labels.hpp"
 #include "Engine.hpp"
 #include "StaticObjAnimation.hpp"
 #include "TypeWriter.hpp"
-#include "loadAssets.hpp"
-#include "MapConfigurator.hpp"
+#include "InGame.hpp"
 #include "MapSelect.hpp"
 #include "GameMenu.hpp"
-#include "InGame.hpp"
+#include "loadAssets.hpp"
 
 
 #endif //CORE_H
