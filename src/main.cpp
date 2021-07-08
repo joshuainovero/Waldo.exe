77d76de885225d6257442849d3dceb1b156f96f1
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-07-08 19:34:58 +0800

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 #include "Core.hpp"
 
 int WINAPI WinMain(HINSTANCE hThisInstance, HINSTANCE hPrevInstance,LPSTR lpszArgument, int nCmdShow){
-	State::states.push_back(new GAMEMENU);
-	State::states.push_back(new MAPSELECT);
-	State::states.push_back(new INGAME);
-	windowLoad();
+	loadingScreen();
 	getMainDirectory();
 	Engine engine;
 	engine.run();
