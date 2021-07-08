e16c97a4937fbeda3a862f31ff75da8dd8ea131f
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-07-08 19:29:03 +0800

    set sprite scales

--- src/State.cpp
+++ src/State.cpp
@@ -2,6 +2,33 @@
 #include <Windows.h>
 std::vector<State*> State::states;
 State *State::currentState;
+sf::Texture State::arrowTexture;
+sf::Texture State::handTexture;
+sf::Sprite State::arrowSprite;
+sf::Sprite State::handSprite;
+sf::SoundBuffer State::globalClickBuffer;
+sf::Sound State::globalClickSound;
+float State::fullScreenSpriteScale;
+float State::scaleArrow;
+
+float State::getFullScreenSpriteScale() { return State::fullScreenSpriteScale; }
+float State::getScaleArrow() { return State::scaleArrow; }
+
+void State::createSprite(sf::Texture &textureP, sf::Sprite &spriteP, const std::string &pathF){
+    EDassets::decryptFile(pathF);
+    increaseBar(BARUI::barload);
+    textureP.loadFromFile("Assets/sprite.png");
+    spriteP.setTexture(textureP);
+    increaseBar(BARUI::barload);
+}
+
+void State::stopCurrentMusicPlaying(){
+    currentMusic->stop();
+}
+
+// void State::loadResources() {
+
+// }
 
 State::~State(){
     for (size_t i = 0; i < State::states.size(); ++i){
--- src/State.hpp
+++ src/State.hpp
@@ -1,11 +1,32 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include "MapProperties.hpp"
 
 class State{
+protected:
+    static sf::Texture arrowTexture;
+    static sf::Texture handTexture;
+    static sf::Sprite arrowSprite;
+    static sf::Sprite handSprite;
+    static sf::SoundBuffer globalClickBuffer;
+    static sf::Sound globalClickSound;
+
+    static float fullScreenSpriteScale;
+    static float scaleArrow;
+    static float getFullScreenSpriteScale();
+    static float getScaleArrow();
+    static constexpr float maxResolution = 1080;
+    sf::Music *currentMusic;
 public:
     static std::vector<State*> states;
     static State *currentState;
     State() = default;
+
+    void createSprite(sf::Texture &textureP, sf::Sprite &spriteP, const std::string &pathF);
+    void stopCurrentMusicPlaying();
+
     virtual ~State();
+    virtual void loadResources() = 0;
     virtual void run(sf::RenderWindow *window, const float &dtArg) = 0;
 };
