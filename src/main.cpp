3d5d952190fe90601c3047f516e740b46b73df98
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-07-10 18:44:54 +0800

    remove static members

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,6 @@
 #include "Engine.hpp"
 
 int WINAPI WinMain(HINSTANCE hThisInstance, HINSTANCE hPrevInstance,LPSTR lpszArgument, int nCmdShow){
-	// #ifdef DEBUG
-	// 	system(std::string("title Waldo Debug " + getDataJson()["app-properties"]["localversion"].asString()).c_str());
-    // 	RECT r;
-    // 	GetWindowRect(GetConsoleWindow(), &r);
-    // 	MoveWindow(GetConsoleWindow(), r.left, r.top, 800, 600, TRUE);
-	// #endif // DEBUG
 	Engine engine;
 	engine.run();
 	return EXIT_SUCCESS;
